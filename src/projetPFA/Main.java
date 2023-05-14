5e418db0a58d2c2ab3cbce3e384190406fe1d0a5
Ameni9 <ameni.triki@cappsule.io>
2023-05-14 19:20:48 +0100

    progress

--- src/projetPFA/Main.java
+++ src/projetPFA/Main.java
@@ -1,5 +1,5 @@
 package projetPFA;
-
+import java.sql.*;
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,11 +10,28 @@ public class Main {
 		Admin a=new Admin("aaa","tk","aaaaa@pi.tn");
 		System.out.println(a.username+" "+a.password+" "+""+a.email);
 
-		Etudiant e = new Etudiant("abc","abc","abc@pi.tn"); 
-		System.out.println(e.username+" "+e.password+" "+""+e.email);
+		Etudiant e1 = new Etudiant("abc","abc","abc@pi.tn"); 
+		System.out.println(e1.username+" "+e1.password+" "+""+e1.email);
 		
 		
 		Login l =new Login();
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://192.168.188.143/login");
+			Statement st =con.createStatement();
+			ResultSet res = st.executeQuery("select * from loginadmin");
+			
+			while(res.next()) {
+				System.out.println("Login:" + res.getString(1));
+			}
+			
+			
+			
+		}catch(Exception e) {
+			System.out.print(e.getMessage());
+		}
+		
 		
 
 	}
